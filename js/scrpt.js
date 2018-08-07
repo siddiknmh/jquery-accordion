dab07b51bc0e03a6cd657dbfebeac8f594126831
AB Siddik <siddiknmh@gmail.com>
2018-08-07 09:38:44 +0600

    jquery toggle on off effect add

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	
-	$('.acc_title').click(function(){
-		
+	$('.acc_title').click(function(){   		
 		var accdata = $(this).attr('acc-data');
 		var selector = '.'+accdata;
-		
-		
 		$(selector).slideToggle();
-		
-		
+		$(this).parent().siblings().each(function() {
+                    if($(this).find('.acc_content').is( ":visible")){
+                       $(this).find('.acc_content').slideToggle();
+                    }
+        });
+   		
 	});
-	
-});
+   });
